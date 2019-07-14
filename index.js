e0587f3f87b2b0bffd11278d08c68dbad704773e
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-14 00:27:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ function add(a,b){
   return a+b
 }
 
-function sub(a,b){
+function subtract(a,b){
   
   return a-b
 }
 
-function mul(a,b){
+function multiply(a,b){
   return a*b
 }
 
-function div(a,b){
+function divide(a,b){
   return a/b
 }
 
@@ -24,34 +24,11 @@ function dec(a){
   return --a
 }
 
-function 
-/*
-it('dec(n) decrements n and returns the result', function() {
-  expect(dec(a)).toEqual(a - 1)
-})
+function makeInt(n){
+  return parseInt('n',10)
+  
+}
 
-describe('makeInt(n)', function() {
-  it('parses n as an integer and returns the parsed integer', function() {
-    expect(makeInt(a.toString())).toEqual(a)
-  })
-
-  it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
-  })
-})
-
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-  })
-})
-
-*/
+function preserveDecimal(n){
+  return parseFloat('n')
+}
