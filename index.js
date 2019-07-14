b4fabddf5203b0bc3dd0be5bc898fe1f17bd8015
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-14 00:17:38 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var number = 10
+ 
+function add5() {
+  var number += 5
+}
+ 
+function divideBy3() {
+  var number /= 3
+}
+ 
+divideBy3()
+ 
+console.log(number) 
+ 
+add5()
+ 
+console.log(number) 
